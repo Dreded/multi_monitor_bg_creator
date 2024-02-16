484b589587ba63c23bb35b31bfd99f1594c94582
Dreded <git@dredx.com>
2024-02-16 00:34:58 -0800

    Formatting

--- main.py
+++ main.py
@@ -3,24 +3,46 @@ import os
 import sys
 import argparse
 
+
 def split_resolutions(res_list):
     output = []
     for res in res_list:
-        item = res.split('x')
+        item = res.split("x")
         if len(item) != 2:
-            raise Exception("Must provide resolution as str or int e.g. '1920x1080' and only 2 numbers per res")
+            raise Exception(
+                "Must provide resolution as str or int e.g. '1920x1080' and only 2 numbers per res"
+            )
         item[0] = int(item[0])
         item[1] = int(item[1])
         output.append(item)
     return output
 
+
 def get_args():
     parser = argparse.ArgumentParser(os.path.basename(__file__))
-    parser.add_argument('image', help="Image to split up")
-    parser.add_argument('-mr','--monitor_res', help="Enter in your monitor resolution[s] '1920x1080' multiple -mr for multiple monitors default = '-mr 1920x1080 -mr 2560x1440 -mr 1920x1080'", default=None,action='append')
-    parser.add_argument('-o','--output', help="Output filename eg '/tmp/image.jpg' (not for use with -m)", required=False)
-    parser.add_argument('-m','--multi_save', help="Save each split image to its own file.", action='store_true')
-    parser.add_argument('--overwrite', help="Overwrite original image file", action='store_true')
+    parser.add_argument("image", help="Image to split up")
+    parser.add_argument(
+        "-mr",
+        "--monitor_res",
+        help="Enter in your monitor resolution[s] '1920x1080' multiple -mr for multiple monitors default = '-mr 1920x1080 -mr 2560x1440 -mr 1920x1080'",
+        default=None,
+        action="append",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Output filename eg '/tmp/image.jpg' (not for use with -m)",
+        required=False,
+    )
+    parser.add_argument(
+        "-m",
+        "--multi_save",
+        help="Save each split image to its own file.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--overwrite", help="Overwrite original image file", action="store_true"
+    )
     args = parser.parse_args()
     return args
 
@@ -30,6 +52,8 @@ args = get_args()
 if args.monitor_res:
     out_res = split_resolutions(args.monitor_res)
 else:
-    out_res = [[1920,1080],[2560,1440],[1920,1080]]
-r = resize.resize.Resizer(args.image,out_res,args.output, args.multi_save,args.overwrite)
+    out_res = [[1920, 1080], [2560, 1440], [1920, 1080]]
+r = resize.resize.Resizer(
+    args.image, out_res, args.output, args.multi_save, args.overwrite
+)
 r.do_default()
--- multi_monitor_bg_creator/resize.py
+++ multi_monitor_bg_creator/resize.py
@@ -3,11 +3,13 @@ import numpy as np
 import os
 import sys
 
-def quit(reason = None):
+
+def quit(reason=None):
     sys.exit(reason)
 
-class Resizer():
-    def __init__(self,image,desired_resolution,ofilename, multi_save, overwrite):
+
+class Resizer:
+    def __init__(self, image, desired_resolution, ofilename, multi_save, overwrite):
         self.orig_full_name = image
         self.check_input_image()
         self.orig_image = cv2.imread(image)
@@ -21,12 +23,14 @@ class Resizer():
         self.overwrite = overwrite
         self.ofilename = ofilename
 
-        #probably won't need height but grab it anyhow
+        # probably won't need height but grab it anyhow
         self.max_width = 0
         self.max_height = 0
         for resolution in desired_resolution:
-            if resolution[0] > self.max_width: self.max_width = resolution[0]
-            if resolution[1] > self.max_height: self.max_height = resolution[1]
+            if resolution[0] > self.max_width:
+                self.max_width = resolution[0]
+            if resolution[1] > self.max_height:
+                self.max_height = resolution[1]
 
     def check_input_image(self):
         if not os.path.isfile(self.orig_full_name):
@@ -37,64 +41,75 @@ class Resizer():
     def split(self):
         height = self.orig_height
 
-        print(f"Splitting into {self.monitor_count} images from {self.orig_width} x {self.orig_height}")
-        edges = np.linspace(0,self.orig_width, self.monitor_count+1)
-        for start,end in zip(edges[:-1].astype(int), edges[1:].astype(int)):
+        print(
+            f"Splitting into {self.monitor_count} images from {self.orig_width} x {self.orig_height}"
+        )
+        edges = np.linspace(0, self.orig_width, self.monitor_count + 1)
+        for start, end in zip(edges[:-1].astype(int), edges[1:].astype(int)):
             self.split_images.append(self.orig_image[0:height, start:end])
 
-    def add_borders(self,resolution,i):
+    def add_borders(self, resolution, i):
         if resolution[0] < self.max_width:
             # I may be wrong but dont think this would ever be desired
             # here for easy addition in future if needed.
             pass
         if resolution[1] < self.max_height:
             print("\tAdding Border to Height(centered)")
-            border_size = int((self.max_height - resolution[1])/2)
-            border_added = cv2.copyMakeBorder(self.split_images[i],border_size,border_size,0,0,cv2.BORDER_CONSTANT)
+            border_size = int((self.max_height - resolution[1]) / 2)
+            border_added = cv2.copyMakeBorder(
+                self.split_images[i],
+                border_size,
+                border_size,
+                0,
+                0,
+                cv2.BORDER_CONSTANT,
+            )
             self.split_images[i] = border_added
-
 
     def resize(self):
         i = 0
         for resolution in self.desired_res:
-            h,w,_ = self.split_images[i].shape
+            h, w, _ = self.split_images[i].shape
             des_h = resolution[1]
             des_w = resolution[0]
-            image_res = [w,h]
+            image_res = [w, h]
             print(f"Image {i}: Desired = {resolution}\t Current = {image_res}")
             if resolution == image_res:
                 print("\tDesired resolution already met, adding borders as needed.")
-                self.add_borders(resolution,i)
+                self.add_borders(resolution, i)
 
             else:
                 print("\tResizing...")
-                resized = cv2.resize(self.split_images[i], (des_w,des_h))
-                h,w,_ = resized.shape
-                image_res = [w,h]
+                resized = cv2.resize(self.split_images[i], (des_w, des_h))
+                h, w, _ = resized.shape
+                image_res = [w, h]
                 print(f"\tResized to: {image_res}")
                 self.split_images[i] = resized
-                self.add_borders(resolution,i)
-            i = i+1
+                self.add_borders(resolution, i)
+            i = i + 1
 
     def wait_or_exit(self):
         while True:
             pressedKey = cv2.waitKey(1) & 0xFF
-            if pressedKey == ord('q'):
-                quit("Image not saved. Press any-key except 'q' to save on preview window.")
-            #255 is no key, this effectively is the elusive any_key
+            if pressedKey == ord("q"):
+                quit(
+                    "Image not saved. Press any-key except 'q' to save on preview window."
+                )
+            # 255 is no key, this effectively is the elusive any_key
             elif pressedKey != 255:
                 break
-            if cv2.getWindowProperty('Output',cv2.WND_PROP_VISIBLE) < 1:        
+            if cv2.getWindowProperty("Output", cv2.WND_PROP_VISIBLE) < 1:
                 cv2.destroyAllWindows()
-                quit("Image not saved. To Save run again and press any key on preview to save.")
-
+                quit(
+                    "Image not saved. To Save run again and press any key on preview to save."
+                )
 
     def rejoin(self):
         joined = cv2.hconcat(self.split_images)
         cv2.imshow("Output", joined)
         self.joined = joined
-        h,w,_ = joined.shape
-        self.output_res = [w,h]
+        h, w, _ = joined.shape
+        self.output_res = [w, h]
         self.wait_or_exit()
 
     def calc_output_res(self):
@@ -104,7 +119,7 @@ class Resizer():
             width += res[0]
             if res[1] > height:
                 height = res[1]
-        return [width,height]
+        return [width, height]
 
     def suggest_filename(self):
         res = f"{self.output_res[0]}x{self.output_res[1]}"
@@ -118,7 +133,7 @@ class Resizer():
         if self.multi_save:
             filenames = []
             for x in range(self.monitor_count):
-                filenames.append(filename.replace(ext,f"_{x}{ext}"))
+                filenames.append(filename.replace(ext, f"_{x}{ext}"))
             return filenames
 
         return [filename]
@@ -136,12 +151,16 @@ class Resizer():
     def set_file_name(self):
         if self.overwrite:
             if self.multi_save:
-                quit("How do you expect to overwrite the original and save to multiple files?")
+                quit(
+                    "How do you expect to overwrite the original and save to multiple files?"
+                )
 
             print(f"You will be overwriting the image at {self.orig_full_name}")
-            print("You will still be able to [q]uit at the image preview without overwrite")
+            print(
+                "You will still be able to [q]uit at the image preview without overwrite"
+            )
             key = input("Enter 'y' to continue: ")
-            if key.upper() != 'Y':
+            if key.upper() != "Y":
                 quit("Exiting, Nothing done")
             else:
                 return [self.orig_full_name]
@@ -157,6 +176,7 @@ class Resizer():
         self.resize()
         self.rejoin()
         self.save(filename)
+
 
 if __name__ == "__main__":
     os.system("clear")
